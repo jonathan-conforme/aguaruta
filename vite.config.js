3115e80fa45b9c535787a562bdca61732753aa14
jonathan <68912218+jonathan-conforme@users.noreply.github.com>
2026-06-30 20:25:32 -0500

    fix: ajuste de rutas base y pwa para produccion

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
+   
+    base: process.env.NODE_ENV === 'production' ? '/aguaruta/public/' : '/',
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
@@ -12,30 +14,31 @@ export default defineConfig({
         }),
         react(),
         VitePWA({
-            registerType: 'autoUpdate', // Actualiza la app automáticamente cuando subas cambios
+            registerType: 'autoUpdate',
             injectRegister: 'auto',
             strategies: 'generateSW',
-            manifest: false,
+
+            manifest: {
                 name: 'AquaRuta Logística',
                 short_name: 'AquaRuta',
                 description: 'Sistema de Gestión y Rutas para Purificadoras de Agua',
-                theme_color: '#4f46e5', // Color índigo de tu botón principal
-                background_color: '#f9fafb', // Gris claro de tu fondo (bg-gray-50)
-                display: 'standalone', // Hace que se abra en pantalla completa sin barra de navegador
+                theme_color: '#4f46e5',
+                background_color: '#f9fafb',
+                display: 'standalone',
                 orientation: 'portrait',
                 icons: [
                     {
-                        src: '/icons/icon-192x192.png',
+                        src: 'icons/icon-192x192.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: '/icons/icon-512x512.png',
+                        src: 'icons/icon-512x512.png',
                         sizes: '512x512',
                         type: 'image/png'
                     }
                 ]
             }
-        )
+        })
     ],
 });
